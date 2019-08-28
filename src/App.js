cd9159d938865d95810870d42ca761c6555d05b7
unknown <7602811+TanGolf@users.noreply.github.com>
2019-08-28 22:03:49 +0100

    added placeholder to input in app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input onChange = {handler} value = {input}/>
+        <input onChange = {handler} value = {input} placeholder = "Try typing something Pig"/>
         <p>
           Hi there, {input}
         </p>
